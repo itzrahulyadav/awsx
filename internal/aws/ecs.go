bb40422e74d9846b83dce9ded0655eec70e82016
Rahul Kumar Yadav <65400893+itzrahulyadav@users.noreply.github.com>
2025-03-28 16:13:03 +0530

    Update ecs.go

--- internal/aws/ecs.go
+++ internal/aws/ecs.go
@@ -1,2 +1,12 @@
 package aws
 
+
+import (   	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	_ "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+
+func GetClusterDetails(ctx)
